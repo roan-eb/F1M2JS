a41c6e9f3cf388f7cba5ebee68b9025737f99ce9
53v <roan.esteban2@gmail.com>
2021-11-30 13:07:58 +0100

    opdracht 2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # F1M2JS
 
 
-[live view](https://32800.hosts1.ma-cloud.nl/f1m2js/les1-background-color/)
+- [opdracht 1 Background](https://32800.hosts1.ma-cloud.nl/f1m2js/les1-background-color/)
+- [opdracht 2 Boodschappen](https://32800.hosts1.ma-cloud.nl/f1m2js/les2-boodschappen/)
--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,22 @@
 console.log("Script is geladen!");
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'rood';
+    fruitvak.appendChild(nieuwElement);
+}
+
+function zetInGroenteLa(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    nieuwElement.className = 'rood';
+    groentevak.appendChild(nieuwElement);
+}
