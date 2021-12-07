320592d8c298c52361bbd07bf3e68a08f837ee5f
53v <roan.esteban2@gmail.com>
2021-12-07 12:43:53 +0100

    les 3

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 - [opdracht 1 Background](https://32800.hosts1.ma-cloud.nl/f1m2js/les1-background-color/)
 - [opdracht 2 Boodschappen](https://32800.hosts1.ma-cloud.nl/f1m2js/les2-boodschappen/)
+- [opdracht 3 Tentoonstelling](https://32800.hosts1.ma-cloud.nl/f1m2js/les3-tentoonstelling/)
---
+++ web/les3-tentoonstelling/script.js
@@ -0,0 +1,29 @@
+const myImage = document.getElementById('myImage');
+const myTitle = document.getElementById('myTitle');
+
+let paintings = [
+    "img/painting0.jpg",
+    "img/painting1.jpg",
+    "img/painting2.jpg",
+    "img/painting3.jpg",
+    "img/painting4.jpg",
+    "img/painting5.jpg"
+];
+
+let titles = [
+    "De schilderijen van van Gogh",
+    "De rode wijngaard",
+    "De sterrennacht",
+    "Sterrennacht boven de Rhône",
+    "Zonnebloemen",
+    "Boerderij in de Provence"
+];
+
+
+
+
+function changeImage(index){
+    myTitle.innerHTML = titles[index];
+    myImage.src = paintings[index];
+}
+
